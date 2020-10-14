463981ff887735edd118f7c774024278e1984fa8
Alain <bluepioupiou@gmail.com>
2020-10-15 01:32:39 +0200

    made work first in-page version that can be hidden

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const app = document.createElement('div');
+app.id = "my-extension-root";
+document.body.appendChild(app);
+ReactDOM.render(<App />, app);
+
+app.style.display = "none";
+
+chrome.runtime.onMessage.addListener(
+   function(request) {
+      if( request.message === "clicked_browser_action") {
+        toggle();
+      }
+   }
 );
+function toggle(){
+   if (app.style.display === "none") {
+     app.style.display = "block";
+   } else{
+     app.style.display = "none";
+   }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
